9ee8b4c32d95c3a7a3cbc980c9a91441a8ed666f
Iron-Cow <uangeji@gmail.com>
2020-09-12 00:28:55 +0300

    token storage

--- angular-blog/src/app/admin/shared/services/auth.service.ts
+++ angular-blog/src/app/admin/shared/services/auth.service.ts
@@ -12,26 +12,41 @@ export class AuthService {
   ){}
 
   get token(): string{
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate){
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     // firebase
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${environment.apiKey}`, user)
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
       );
   }
 
   logout(): void{
-
+    this.setToken(null);
   }
 
   is_authenticated(): boolean{
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse): void {
+  // @ts-ignore
+  private setToken(response: FbAuthResponse | null): void {
     console.log(response);
+    if (response){
+    const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);  // convert to ms
+    localStorage.setItem('fb-token', response.idToken);
+    localStorage.setItem('fb-token-exp', expDate.toString());}
+    else{
+      localStorage.clear();
+    }
+
   }
 }
